a3b8207bc962c372d95797ec49a8da07301f0c8c
Amit Chaudhary <amitchaudhary0539@outlook.com>
2026-09-14 17:34:01 +0530

    feat(server): wire group and expense routes, enable FK

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,12 +8,13 @@ import (
 	"os"
 
 	"github.com/amit9838/splitwise-backend-go/internal/category"
+	"github.com/amit9838/splitwise-backend-go/internal/expense"
+	"github.com/amit9838/splitwise-backend-go/internal/group"
 	_ "modernc.org/sqlite"
 )
 
 func openDatabase(dbPath string) (*sql.DB, error) {
-	db, err := sql.Open("sqlite", dbPath)
-	// db.Exec("PRAGMA foreign_keys = ON;")
+	db, err := sql.Open("sqlite", dbPath+"?_foreign_keys=1") // enable fk constraint
 	if err != nil {
 		return nil, err
 	}
@@ -55,9 +56,11 @@ func main() {
 	if err := catStore.InitSchema(); err != nil {
 		log.Fatalf("failed to initialize category schema: %v", err)
 	}
+	if err := catStore.CreateIndexes(); err != nil {
+		log.Fatalf("failed to create category indexes: %v", err)
+	}
 
 	// handler := setupRouter()
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
 	mux.HandleFunc("GET /health", healthHandler)
@@ -69,6 +72,39 @@ func main() {
 	mux.HandleFunc("GET /categories/{group_id}/{id}", categoryHandler.Get)
 	mux.HandleFunc("PUT /categories/{group_id}/{id}", categoryHandler.Update)
 	mux.HandleFunc("DELETE /categories/{group_id}/{id}", categoryHandler.Delete)
+
+	// --------------- grp ------------
+	grpStore := group.NewDBStore(db)
+	if err := grpStore.InitSchema(); err != nil {
+		log.Fatalf("failed to initialize group schema: %v", err)
+	}
+	if err := grpStore.CreateIndexes(); err != nil {
+		log.Fatalf("failed to create group indexes: %v", err)
+	}
+	groupHandler := group.NewHandler(group.NewService(grpStore))
+
+	// group
+	mux.HandleFunc("POST /groups", groupHandler.Create)
+	mux.HandleFunc("GET /groups", groupHandler.List)
+	mux.HandleFunc("GET /groups/{id}", groupHandler.Get)
+	mux.HandleFunc("PUT /groups/{id}", groupHandler.Update)
+	mux.HandleFunc("DELETE /groups/{id}", groupHandler.Delete)
+
+	// Expense
+	expStore := expense.NewDBStore(db)
+	if err := expStore.InitSchema(); err != nil {
+		log.Fatalf("failed to initialize expense schema: %v", err)
+	}
+	if err := expStore.CreateIndexes(); err != nil {
+		log.Fatalf("failed to create expense indexes: %v", err)
+	}
+	expenseHandler := expense.NewHandler(expense.NewService(expStore))
+	// expense
+	mux.HandleFunc("POST /expenses", expenseHandler.Create)
+	mux.HandleFunc("GET /expenses/group/{group_id}", expenseHandler.List)
+	mux.HandleFunc("GET /expenses/{id}", expenseHandler.Get)
+	mux.HandleFunc("PUT /expenses/{id}", expenseHandler.Update)
+	mux.HandleFunc("DELETE /expenses/{id}", expenseHandler.Delete)
 
 	port := "8080"
 	server := &http.Server{
